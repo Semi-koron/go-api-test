324c032035b810cd734813a07406fcc641ffa255
Semi-koron <6jimoto6@gmail.com>
2026-06-02 00:45:52 +0900

    migrationファイル作成

---
+++ db/migrations/000001_users.up.sql
@@ -0,0 +1,15 @@
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+)
+
+CREATE TABLE follow (
+    follower_user_id VARCHAR(255) NOT NULL,
+    followee_user_id VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (follower_id, followee_id)
+);
